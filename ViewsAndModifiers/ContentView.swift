c52e4356dff8479bef2dc86842f7acf36a9de548
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-06-30 23:18:21 -0700

    mainview

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,15 +7,74 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+
+struct Buttonn: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.black)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .fontWeight(.semibold)
+            .font(.title)
+    }
+}
+
+extension View {
+    func button() -> some View {
+        modifier(Buttonn())
+    }
+}
+
+struct ContentView: View { // Homepage
+    @State private var showCredits = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Image("Moodle")
+                
+                Text("MOODLE")
+                    .font(.largeTitle)
+                    .fontDesign(.rounded)
+                    .fontWeight(.heavy)
+                
+                Spacer()
+                
+                Button("Cards") { }
+                    .button()
+                Button("Decks/Study") { }
+                    .button()
+                Button("Credits") {
+                    showCredits = true
+                }
+                .button()
+                
+                Spacer()
+                Spacer()
+            }
+            .padding().padding().padding().padding()
+            .background(.quinary, ignoresSafeAreaEdges: .all)
+            .sheet(isPresented: $showCredits) {
+                Credits()
+            }
+        }
+    }
+}
+
+struct Credits: View {
     var body: some View {
         VStack {
-            Image(systemName: "gearshape.fill")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Drag here to exit view!")
+                .padding()
+            
+            Spacer()
+            Text("Hello!")
+            Spacer()
         }
-        .padding()
     }
 }
 
