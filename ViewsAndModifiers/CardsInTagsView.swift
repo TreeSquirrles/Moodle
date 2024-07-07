f76830e65be14e603adbf7d3dd7d8dd003082a94
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-07-07 00:49:37 -0700

    nvm my dad fixed it

--- ViewsAndModifiers/CardsInTagsView.swift
+++ ViewsAndModifiers/CardsInTagsView.swift
@@ -10,24 +10,25 @@ import SwiftData
 
 struct CardsInTagsView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var cards: [Card]
+    //@Query var cards: [Card]
+    //@Query var tags: [Tag]
     
     @Bindable var tag: Tag
-    @State private var path = [Card]()
+    //@State private var path = [Card]()
     
     
     
     var body: some View {
         //Text("Hello, World!")
         List {
-            ForEach(cards) { card in
+            ForEach(tag.cards) { card in
                 NavigationLink(value: card) {
                     VStack(alignment: .leading) {
                         Text(card.front)
                             .font(.headline)
                         
                         Text(card.dateAdded.formatted())
-                        Text("\(cards.count)")
+                        //("\(tag.cards.count)")
                     }
                 }
             }
@@ -61,22 +62,36 @@ struct CardsInTagsView: View {
         print("====")
         print(self.tag.tagName)
         print("----")
-        _cards = Query()
-//        _cards = Query(filter: #Predicate {
-//            if searchString.isEmpty {
-//                return true
+        
+        
+        //_cards = Query()
+//
+//        _tags = Query(filter: #Predicate {
+//            if taginput.tagName.isEmpty {
+//                return false
 //            } else {
-//                return $0.front.localizedStandardContains(searchString)
+//                return $0.tagName.localizedStandardContains( taginput.tagName)
 //            }
-//        }, sort: [sort])
-//        print(cards.count)
-        //path = [cards[1]]
+//        })
+        
+        
+        //var bbb = false
+//        _cards = Query(filter: #Predicate {
+//            //bbb = false
+////            ForEach($0.tags) { tag in
+////                if tag.tagName.localizedStandardContains( taginput.tagName) {
+////                    bbb = true
+////                }
+////            }
+//            return true
+//        })
     }
     
+
     
     func removeCardsFromTag(_ indexSet: IndexSet) {
         for index in indexSet {
-            let card = cards[index]
+            let card = tag.cards[index]
             // // Do something to remove card from the deck
             //modelContext.delete(card)
             
