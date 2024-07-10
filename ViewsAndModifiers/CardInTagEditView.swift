93d93cae53d22e2cc022e235dca77faa909e5059
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-07-10 12:12:00 -0700

    yolo

---
+++ ViewsAndModifiers/CardInTagEditView.swift
@@ -0,0 +1,18 @@
+//
+//  CardInTagEditView.swift
+//  ViewsAndModifiers
+//
+//  Created by Seungyoo Kim-Jung on 7/8/24.
+//
+
+import SwiftUI
+
+struct CardInTagEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardInTagEditView()
+}
